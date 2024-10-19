c36eb0349533609bc6e3c80ecf8ead6126920461
Elanz <kontak.elan21@gmail.com>
2024-10-19 13:37:06 +0700

    first commit 🚀

---
+++ src/Pojokmanga/Pojokmanga.ts
@@ -0,0 +1,44 @@
+import {
+  ContentRating,
+  SourceInfo,
+  BadgeColor,
+  SourceIntents,
+} from "@paperback/types";
+
+import { getExportVersion, Madara } from "../Madara";
+
+const DOMAIN = "https://pojokmanga.info";
+
+export const PojokmangaInfo: SourceInfo = {
+  version: getExportVersion("0.0.3"),
+  name: "Pojokmanga",
+  description: `Extension that pulls manga from ${DOMAIN}`,
+  author: "ElanErlangga",
+  authorWebsite: "https://github.com/ElanErlangga",
+  icon: "icon.png",
+  contentRating: ContentRating.EVERYONE,
+  websiteBaseURL: DOMAIN,
+  sourceTags: [
+    {
+      text: "Indonesian",
+      type: BadgeColor.GREY,
+    },
+  ],
+  intents:
+    SourceIntents.MANGA_CHAPTERS |
+    SourceIntents.HOMEPAGE_SECTIONS |
+    SourceIntents.CLOUDFLARE_BYPASS_REQUIRED |
+    SourceIntents.SETTINGS_UI,
+};
+
+export class Pojokmanga extends Madara {
+  baseUrl: string = DOMAIN;
+
+  override alternativeChapterAjaxEndpoint = true;
+
+  override hasAdvancedSearchPage = true;
+
+  override hasProtectedChapters = true;
+
+  override directoryPath = "komik";
+}
---
+++ src/Shinigami/Shinigami.ts
@@ -0,0 +1,42 @@
+import {
+  ContentRating,
+  SourceInfo,
+  BadgeColor,
+  SourceIntents,
+} from "@paperback/types";
+
+import { getExportVersion, Madara } from "../Madara";
+
+const DOMAIN = "https://shinigami05.com";
+
+export const ShinigamiInfo: SourceInfo = {
+  version: getExportVersion("0.0.7"),
+  name: "Shinigami",
+  description: `Extension that pulls manga from ${DOMAIN}`,
+  author: "ElanErlangga",
+  authorWebsite: "https://github.com/ElanErlangga",
+  icon: "icon.png",
+  contentRating: ContentRating.EVERYONE,
+  websiteBaseURL: DOMAIN,
+  sourceTags: [
+    {
+      text: "Indonesian",
+      type: BadgeColor.GREY,
+    },
+  ],
+  intents:
+    SourceIntents.MANGA_CHAPTERS |
+    SourceIntents.HOMEPAGE_SECTIONS |
+    SourceIntents.CLOUDFLARE_BYPASS_REQUIRED |
+    SourceIntents.SETTINGS_UI,
+};
+
+export class Shinigami extends Madara {
+  baseUrl: string = DOMAIN;
+
+  override alternativeChapterAjaxEndpoint = true;
+
+  override hasAdvancedSearchPage = true;
+
+  override directoryPath = "series";
+}
