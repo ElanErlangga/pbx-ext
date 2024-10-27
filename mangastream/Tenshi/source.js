7726cd3377e1655ee76b7e0d9c60e1a37d7f2c6b
ElanErlangga <31978138+ElanErlangga@users.noreply.github.com>
2024-10-27 08:20:55 +0000

    Deploying to gh-pages from @ ElanErlangga/pbx-ext@b31be387958e5da3a15a9185de4e90fb8393c590 🚀

--- mangastream/Tenshi/source.js
+++ mangastream/Tenshi/source.js
@@ -2306,7 +2306,6 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
     constructor() {
       super(...arguments);
       this.baseUrl = DOMAIN;
-      this.directoryPath = "komik";
       this.manga_tag_selector_box = "div.seriestugenre";
       this.dateMonths = {
         january: "Januari",
