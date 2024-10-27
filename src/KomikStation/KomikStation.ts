b36ddcf0c757aaf8ea61303c0085a2f097589ff1
Elan Erlanggga <31978138+ElanErlangga@users.noreply.github.com>
2024-10-27 13:39:10 +0700

    Fix KomikStation.ts

--- src/KomikStation/KomikStation.ts
+++ src/KomikStation/KomikStation.ts
@@ -12,7 +12,7 @@ import {
 
 const DOMAIN = 'https://komikstation.co'
 
-export const TenshiInfo: SourceInfo = {
+export const KomikStationInfo: SourceInfo = {
     version: getExportVersion('0.0.1'),
     name: 'Tenshi',
     description: `Extension that pulls manga from ${DOMAIN}`,
@@ -30,7 +30,7 @@ export const TenshiInfo: SourceInfo = {
     ]
 }
 
-export class Tenshi extends MangaStream {
+export class KomikStation extends MangaStream {
 
     baseUrl: string = DOMAIN
 
