cffb4f708a65a3d3f1297127c17ec62ff8ad057b
Elanz <kontak.elan21@gmail.com>
2024-10-27 15:00:31 +0700

    Add KomikStation

---
+++ src/KomikStation/KomikStation.ts
@@ -0,0 +1,46 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://komikstation.co'
+
+export const KomikStationInfo: SourceInfo = {
+    version: getExportVersion('0.0.1'),
+    name: 'KomikStation',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'ElanErlangga',
+    authorWebsite: 'http://github.com/ElanErlangga',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: [
+    {
+        text: "Indonesia",
+        type: BadgeColor.GREY
+    },
+    ]
+}
+
+export class KomikStation extends MangaStream {
+
+    baseUrl: string = DOMAIN
+
+    override configureSections(): void {
+        this.homescreen_sections['new_titles'].enabled = false
+        this.homescreen_sections['top_alltime'].enabled = false
+        this.homescreen_sections['top_monthly'].enabled = false
+        this.homescreen_sections['top_weekly'].enabled = false
+        this.homescreen_sections['popular_today'].selectorFunc = ($: CheerioStatic) => $('div.bsx', $('h2:contains(Yang Lagi Trending!)')?.parent()?.next())
+        this.homescreen_sections['latest_update'].selectorFunc = ($: CheerioStatic) => $('div.bsx', $('h2:contains(Rilisan Terbaru)')?.parent()?.next())
+        this.homescreen_sections['latest_update'].subtitleSelectorFunc = ($: CheerioStatic, element: CheerioElement) => $('span.fivchap', element).first().text().trim()
+    }
+}
