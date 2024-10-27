6bca980cc1e5e6a26e25d854544031cea8bc5303
ElanErlangga <31978138+ElanErlangga@users.noreply.github.com>
2024-10-27 08:36:06 +0000

    Deploying to gh-pages from @ ElanErlangga/pbx-ext@044b28ef6d9651e83046d5b6c23c03c3b40bd2bd 🚀

--- mangastream/KomikStation/source.js
+++ mangastream/KomikStation/source.js
@@ -2306,29 +2306,9 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
     constructor() {
       super(...arguments);
       this.baseUrl = DOMAIN;
-      this.dateMonths = {
-        january: "Januari",
-        february: "Februari",
-        march: "Maret",
-        april: "April",
-        may: "Mei",
-        june: "Juni",
-        july: "Juli",
-        august: "Agustus",
-        september: "September",
-        october: "Oktober",
-        november: "November",
-        december: "Desember"
-      };
     }
     configureSections() {
       this.homescreen_sections["new_titles"].enabled = false;
-      this.homescreen_sections["top_alltime"].enabled = false;
-      this.homescreen_sections["top_monthly"].enabled = false;
-      this.homescreen_sections["top_weekly"].enabled = false;
-      this.homescreen_sections["popular_today"].selectorFunc = ($) => $("div.bsx", $("h2:contains(Terpopuler Hari Ini)")?.parent()?.next());
-      this.homescreen_sections["latest_update"].selectorFunc = ($) => $("div.bsx", $("h2:contains(Rilisan Terbaru)")?.parent()?.next());
-      this.homescreen_sections["latest_update"].subtitleSelectorFunc = ($, element) => $("span.fivchap", element).first().text().trim();
     }
   };
   return __toCommonJS(KomikStation_exports);
